61d1269107c04a9132df0c26ff1befe104b09133
wangzlgithob1 <765991751@qq.com>
2019-03-20 22:30:32 +0800

    update 1

---
+++ src/main/java/com/crawler/wallstreetcnnewsforex/WallstreetcnNewsForexApplication.java
@@ -0,0 +1,13 @@
+package com.crawler.wallstreetcnnewsforex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WallstreetcnNewsForexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WallstreetcnNewsForexApplication.class, args);
+    }
+
+}
